4a9dda3e51be7fe94063acb51f274b985eaac8be
João Gabriel Miranda <jg.miranda@outlook.com>
2017-08-02 00:43:03 -0300

    criação de header generico na classe stationservice; finalização da listagem de estações;

--- rent-a-thing-ionic/src/services/stationservice.ts
+++ rent-a-thing-ionic/src/services/stationservice.ts
@@ -1,8 +1,9 @@
-import { Injectable, Inject } from '@angular/core';
+﻿import { Injectable, Inject } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 
 @Injectable()
-export class StationService{
+export class StationService {
+    headers: Headers;
     api_url: string;
     apikey: string;
     http: Http;
@@ -11,26 +12,31 @@ export class StationService{
         return [[Http]];
     }
 
-    constructor(http: Http){
+    constructor(http: Http) {
         this.http = http;
         this.apikey = window.localStorage.getItem('api-key');
 
-        this.api_url = 'http://127.0.0.1:8087/'
+        //this.api_url = 'http://127.0.0.1:8087/'
+        this.api_url = 'http://192.168.0.107:8087/';
+
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Authorization', 'Token ' + this.apikey);
     }
 
     getStations() {
-        var headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Authorization', 'Token ' + this.apikey);
-
         return new Promise(resolve => {
-            this.http.get(this.api_url + 'server/api/clients', { headers: headers})
-            .subscribe(data => {
-                if(data.ok){
-                    resolve(data.json());
-                }
-            }, err => {
-            });
+            this.http.get(this.api_url + 'server/api/clients', { headers: this.headers })
+                .subscribe(data => {
+                    if (data.ok) {
+                        resolve(data.json());
+                    }
+                }, err => {
+                });
         });
+    }
+
+    getStation(id) {
+
     }
 }
